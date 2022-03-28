510bd4af8cb032e155153a4d3485e93f6b96915c
Tom Sisk <tom.sisk@gmail.com>
2022-03-28 16:35:48 -0500

    add ssm helper

--- install.sh
+++ install.sh
@@ -6,4 +6,5 @@ SCRIPT_PATH="$(cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P)"
 ln -s $SCRIPT_PATH/ec2-exec /usr/local/bin/ec2-exec
 ln -s $SCRIPT_PATH/ec2-ls /usr/local/bin/ec2-ls
 ln -s $SCRIPT_PATH/ec2-ssh /usr/local/bin/ec2-ssh
+ln -s $SCRIPT_PATH/ec2-ssm /usr/local/bin/ec2-ssm
 
